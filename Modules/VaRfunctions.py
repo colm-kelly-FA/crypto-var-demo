1bd2defe9d7f496c38966aae270b468921f5480e
colm-kelly-FA <colm.kelly@funds-axis.com>
2022-07-05 22:54:26 +0100

    Introduced Modules file and ability to load custome functions from seperate file

--- Modules/VaRfunctions.py
+++ Modules/VaRfunctions.py
@@ -1,8 +1,16 @@
+print("I am being executed!")
+
 import numpy as np 
 import pandas as pd
+from scipy import interpolate
+import copy
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from arch import arch_model
+from scipy.stats import norm
 
 
-print("I am being executed!")
+
+
 
 
 def weighted_hist_VaR_2(coin, alpha, history, lambda_,exponential_weighted=True, mass_centered=True):
@@ -119,3 +127,71 @@ def weighted_hist_VaR_2(coin, alpha, history, lambda_,exponential_weighted=True,
         coin_with_var["EqW VaR"] = VaR_column
     
     return coin_with_var
+
+
+def garch_VaR(coin, alpha, history, ar_order, garch_order=(1,1), horizon=1, dist="NORM"):
+    
+    if coin.index.names[0] == "Symbol":
+        coin_data = coin.droplevel("Symbol").loc[:,["Return"]].dropna()
+    else:
+        coin_data = coin.loc[:,["Return"]].dropna()
+        
+    coin_data = coin_data.sort_values(by=["Date"], ascending = True)
+    date_index = coin_data.index.get_level_values("Date")
+    
+    nforecasts = horizon
+    
+    # data and window size
+    n_obs = len(coin_data)
+    window = history
+    window_break_init = date_index[window-1]
+    window_break = window_break_init
+    
+    # ar_order = (2, 0, 1)
+    # garch_p = 2
+    # garch_q = 2
+    
+    # ARIMA training and in-sample residuals
+    ar_mod = SARIMAX(coin_data[:window_break], order = ar_order)
+    ar_mod_results = ar_mod.fit()
+    coin_data["yhat"] = ar_mod_results.fittedvalues
+    coin_data["ar_resid"] = ar_mod_results.resid
+    
+    # first out of sample AR forecast and residual
+    coin_data.loc[date_index[window],"yhat"] = ar_mod_results.forecast(steps=nforecasts).values
+    coin_data.loc[date_index[window],"ar_resid"] = coin_data.loc[date_index[window],"Return"]-coin_data.loc[date_index[window],"yhat"]
+
+    # Garch Model
+    resid_model = arch_model(coin_data.loc[:window_break,"ar_resid"], p = garch_order[0], q = garch_order[1], vol = "GARCH")
+    resid_model_results = resid_model.fit(last_obs=window_break, disp="off")
+
+    coin_data.loc[:, "GARCH vol"] = resid_model_results.forecast(start=window_break, horizon=nforecasts).residual_variance
+    
+    t=0
+
+    while window_break < date_index[len(date_index)-2]:
+        
+        # increment
+        t = t+1
+        window_break = date_index[window-1 + t]
+
+        # re-train garch model on expanded window 
+        resid_model = arch_model(coin_data.loc[:window_break,"ar_resid"], p = garch_order[0], q = garch_order[1], vol = "GARCH")
+        resid_model_results = resid_model.fit(last_obs=window_break, disp="off")
+
+        # store garch variance
+        coin_data.loc[window_break, "GARCH vol"] = resid_model_results.forecast(start=window_break, horizon=nforecasts).residual_variance.values[-1]
+
+        # re-train AR model on expanding window
+        ar_mod = SARIMAX(coin_data.loc[:window_break, ["Return"]], order = ar_order)
+        ar_mod_results = ar_mod.fit()
+
+        coin_data.loc[date_index[window + t], "yhat"] = ar_mod_results.forecast(1).values
+        coin_data.loc[date_index[window + t], "ar_resid"]  = coin_data.loc[date_index[window + t],"Return"]-coin_data.loc[date_index[window + t],"yhat"]
+
+    coin_data["GARCH vol"] = coin_data["GARCH vol"].shift(1)
+    
+    distribution_quantile = norm.ppf(alpha)
+    coin_data["GARCH VaR"] = -coin_data["yhat"]+distribution_quantile*np.sqrt(coin_data["GARCH vol"])
+
+    return coin_data
